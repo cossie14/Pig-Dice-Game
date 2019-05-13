0a818005353a7cd21326edfa7e98f7eb96eddd76
sylviah <sylviah.rutto@gmail.com>
2019-05-13 12:05:54 +0300

    added changes

--- js/jquery-3.4.0.js
+++ js/jquery-3.4.0.js
@@ -1,14 +1,14 @@
-$(document).ready(function() {
-  $(".clickable").click(function() {
-    $("#showing").slideToggle();
-    $("#hidden").slideToggle();
-  });
-
-  $(".clickable-d").click(function() {
-    $("#showing-d").slideToggle();
-    $("#hidden-d").slideToggle();
-  });
-
-  $(".clickable-p").click(function() {
-    $("#showing-p").slideToggle();
-    $("#hidden-p").slideToggle();
+// $(document).ready(function() {
+//   $(".clickable").click(function() {
+//     $("#showing").slideToggle();
+//     $("#hidden").slideToggle();
+//   });
+//
+//   $(".clickable-d").click(function() {
+//     $("#showing-d").slideToggle();
+//     $("#hidden-d").slideToggle();
+//   });
+//
+//   $(".clickable-p").click(function() {
+//     $("#showing-p").slideToggle();
+//     $("#hidden-p").slideToggle();
